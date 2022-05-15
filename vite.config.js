bdb1b8980df3e8c9c9ad4e215cc981abf87aaf3d
Jose Monroy <mnry7999@gmail.com>
2022-05-15 10:01:57 -0500

    modify path

--- vite.config.js
+++ vite.config.js
@@ -5,10 +5,11 @@ import vue from '@vitejs/plugin-vue'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
-  resolve: {
-    alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+    plugins: [vue()],
+    resolve: {
+        alias: {
+            '@': fileURLToPath(new URL('https://joseluismonroy.github.io/The_Submergers/',
+                import.meta.url))
+        }
     }
-  }
 })
