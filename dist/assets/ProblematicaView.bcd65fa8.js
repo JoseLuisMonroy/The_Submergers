1a3927bd0add63f041fa8d1323be12346086ed20
Jose Monroy <mnry7999@gmail.com>
2022-05-15 14:04:55 -0500

    Uptdate compiled files

--- dist/assets/ProblematicaView.bcd65fa8.js
+++
@@ -1,1 +0,0 @@
-import{_ as a,o as _,c as o,p as t,a as c,b as s}from"./index.6cb1bb05.js";const r={},d=e=>(t("data-v-a0da39e8"),e=e(),c(),e),p={class:"wrapper"},n=d(()=>s("h1",null,"VISTA DE PROBLEMATICAS",-1)),i=[n];function l(e,u){return _(),o("div",p,i)}var v=a(r,[["render",l],["__scopeId","data-v-a0da39e8"]]);export{v as default};
